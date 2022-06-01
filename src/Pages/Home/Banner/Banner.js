8097ebae7fda60cb3e04a556a7bd29e51eb0a09f
najuml-bd <najmul0612@gmail.com>
2022-06-01 10:05:20 +0600

    login & registration component added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import banner1 from '../../../assets/banner/banner1.jpg'
+import Navbar from '../Navbar/Navbar';
 import './Banner.css'
 
 const Banner = () => {
@@ -9,7 +10,10 @@ const Banner = () => {
             <div class="hero min-h-screen" banner-container>
                 <img src={banner1} style={{ backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }} alt="" />
                 <div class="hero-overlay bg-opacity-60"></div>
+                <Navbar />
+
                 <div class="hero-content text-center text-neutral-content">
+
                     <div class="max-w-md">
                         <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
                         <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
